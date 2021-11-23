3b0718e70585db1d0036d395b40fe539b77c30b1
FitrioDermawan <91893718+FitrioDermawan@users.noreply.github.com>
2021-11-23 12:21:05 +0700

    Penambahan edit dan hapus

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,9 @@ Route::post('hasil',"ViewController@hasilPeriksa") ;
 Route::get('/pegawai','PegawaiController@index');
 Route::get('/pegawai/tambah','PegawaiController@tambah');
 Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
 
 
 
