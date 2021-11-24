5311d447d2e6360e2e174236434ca2fadceb9ab3
FitrioDermawan <91893718+FitrioDermawan@users.noreply.github.com>
2021-11-24 18:28:17 +0700

    Pembuatan CRUD pendapatan
    tugas laravel asinkron

---
+++ resources/views/pendapatan/edit.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD Pendapatan</title>
+</head>
+<body>
+	<h3>Data Pendapatan</h3>
+
+	<a href="/pendapatan"> Kembali</a>
+
+	<br/>
+	<br/>
+
+	@foreach($pendapatan as $p)
+	<form action="/pendapatan/update" method="post">
+		{{ csrf_field() }}
+		<input type="hidden" name="id" value="{{ $p->pendapatan_id }}"> <br/>
+		ID Pegawai <input type="number" name="idpegawai" required="required" value="{{ $p->pendapatan_id_pegawai }}"> <br/>
+		Bulan <input type="number" name="bulan" required="required" value="{{ $p->pendapatan_bulan }}"> <br/>
+		Tahun <input type="text" name="tahun" required="required" maxlength="4" value="{{ $p->pendapatan_tahun }}"> <br/>
+		Gaji <input type="number" name="gaji" required="required" value="{{ $p->pendapatan_gaji }}"> <br/>
+        Tunjangan <input type="number" name="tunjangan" required="required" value="{{ $p->pendapatan_tunjangan }}"> <br/>
+		<input type="submit" value="Simpan Data">
+	</form>
+	@endforeach
+
+
+</body>
+</html>
